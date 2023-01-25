c2321587c5d67bdca36bae6b6cbb774a1330ef9c
Zhandos <zh.zhanybek@gmail.com>
2023-01-26 01:43:08 +0600

    adding my js file

---
+++ bin/gendiff.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+import { Command } from 'commander/esm.mjs';
+const program = new Command();
+
+program
+    .description('Compares two configuration files and shows a difference.')
+    .version('0.0.1', '-V, --version', 'output the version number')
+    .arguments('<filepath1> <filepath2>');
+
+program
+  .option('-f, --format <type>',  'output format')
+
+program.parse();
