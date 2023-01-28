385e679a295a4d8e507c7e036625279f6f78c870
Zhandos <zh.zhanybek@gmail.com>
2023-01-29 03:16:03 +0600

    changes in gendiff - second step of project

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,13 +1,14 @@
 #!/usr/bin/env node
 import { Command } from 'commander/esm.mjs';
+
 const program = new Command();
 
 program
-    .description('Compares two configuration files and shows a difference.')
-    .version('0.0.1', '-V, --version', 'output the version number')
-    .arguments('<filepath1> <filepath2>');
+  .description('Compares two configuration files and shows a difference.')
+  .version('0.0.1', '-V, --version', 'output the version number')
+  .arguments('<filepath1> <filepath2>');
 
 program
-  .option('-f, --format <type>',  'output format')
+  .option('-f, --format <type>', 'output format');
 
 program.parse();
