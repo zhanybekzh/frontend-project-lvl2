c68e6ff44dd37978822f2c437e447bd9aceade94
Zhandos <zh.zhanybek@gmail.com>
2023-01-31 07:21:21 +0600

    Шаг 3. Функция для получения файлов и сравнения

---
+++ src/lib/genDiggFunc.js
@@ -0,0 +1,6 @@
+const genDiffFunc = (pathFile1, pathFile2) => {
+    const currendDirectory = process.cwd();
+    const absolutePathFile1 = path.resolve(currendDirectory, pathFile1);
+    const absolutePathFile2 = path.resolve(currendDirectory, pathFile1);
+};
+export default genDiffFunc;
