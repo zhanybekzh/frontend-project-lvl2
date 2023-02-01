2e8035b2f64d1a0b03828c192d843c66895fc8ea
Zhandos <zh.zhanybek@gmail.com>
2023-02-02 02:52:37 +0600

    Создал файл и перенес в другую папку. Основная функция находится в этом файле

---
+++ src/lib/genDiffFunc.js
@@ -0,0 +1,53 @@
+import { readFileSync } from 'node:fs';
+import path from 'path';
+import _ from 'lodash';
+
+
+const genDiffFunc = (pathFile1, pathFile2) => {
+    const currendDirectory = process.cwd();
+    const absolutePathFile1 = path.resolve(currendDirectory, pathFile1);
+    const absolutePathFile2 = path.resolve(currendDirectory, pathFile2);
+    const contentFile1 = readFileSync(absolutePathFile1);
+    const contentFile2 = readFileSync(absolutePathFile2);
+    const objFile1 = JSON.parse(contentFile1);
+    const objFile2 = JSON.parse(contentFile2);
+    const keys1 = Object.keys(objFile1);
+    const keys2 = Object.keys(objFile2);
+    const keysList = _.sortBy(_.union(keys1, keys2));
+
+    const analyzedArray = keysList.map((key) => {
+        if(!_.has(objFile2, key)) {
+            return { key, value: objFile1[key], state: 'deleted' };
+        }
+        if (!_.has(objFile1, key)) {
+            return { key, value: objFile2[key], state: 'added' };
+        }
+        if (_.has(objFile1, key) && _.has(objFile2, key) && objFile1[key] === objFile2[key]) {
+          return { key, value: objFile1[key], state: 'unchanged' };
+        }
+        return {
+          key, oldValue: objFile1[key], newValue: objFile2[key], state: 'changed',
+        };
+    });
+    let result = '{\n';
+    const analyzedBody = analyzedArray.map((line) => {
+      if (line.state === 'deleted') {
+        return `    - ${line.key}: ${line.value}`;
+      }
+      if (line.state === 'added') {
+        return `    + ${line.key}: ${line.value}`;
+      }
+      if (line.state === 'unchanged') {
+        return `      ${line.key}: ${line.value}`;
+      }
+      if (line.state === 'changed') {
+        return `    - ${line.key}: ${line.oldValue}\n    + ${line.key}: ${line.newValue}`;
+      }
+    });
+    result += analyzedBody.join('\n');
+    result += '\n}';
+    console.log(result);
+
+};
+
+export default genDiffFunc;
--- src/lib/genDiggFunc.js
+++
@@ -1,6 +0,0 @@
-const genDiffFunc = (pathFile1, pathFile2) => {
-    const currendDirectory = process.cwd();
-    const absolutePathFile1 = path.resolve(currendDirectory, pathFile1);
-    const absolutePathFile2 = path.resolve(currendDirectory, pathFile1);
-};
-export default genDiffFunc;
