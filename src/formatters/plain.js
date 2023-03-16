e4ce040700adbef2bdf3d60ad26f10b2e308ee03
Zhandos <zh.zhanybek@gmail.com>
2023-03-17 01:08:11 +0600

    fix notes from mentor

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -10,26 +10,28 @@ const stringify = (value) => {
   return value;
 };
 
-const plain = (ast, path = '') => {
-  const content = ast.map((child) => {
-    const name = path ? `${path}.${child.key}` : child.key;
-
-    switch (child.state) {
-      case 'deleted':
-        return `Property '${name}' was removed`;
-      case 'added':
-        return `Property '${name}' was added with value: ${stringify(child.value)}`;
-      case 'changed':
-        return `Property '${name}' was updated. From ${stringify(child.valueOfFirstFile)} to ${stringify(child.valueOfSecondFile)}`;
-      case 'tree':
-        return plain(child.tree, name);
-      case 'unchanged':
-        return '';
-      default:
-        throw new Error(`Unknown child state: '${child.state}'!`);
-    }
-  });
-  return content.filter((item) => item).join('\n');
+const plain = (ast) => {
+  const iter = (node, path) => {
+    const content = node.map((child) => {
+      const name = path ? `${path}.${child.key}` : child.key;
+      switch (child.state) {
+        case 'deleted':
+          return `Property '${name}' was removed`;
+        case 'added':
+          return `Property '${name}' was added with value: ${stringify(child.value)}`;
+        case 'changed':
+          return `Property '${name}' was updated. From ${stringify(child.valueOfFirstFile)} to ${stringify(child.valueOfSecondFile)}`;
+        case 'tree':
+          return iter(child.tree, name);
+        case 'unchanged':
+          return '';
+        default:
+          throw new Error(`Unknown child state: '${child.state}'!`);
+      }
+    });
+    return content.filter((item) => item).join('\n');
+  };
+  return iter(ast, '');
 };
 
 export default plain;
--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -15,27 +15,27 @@ const stringify = (value, depth) => {
 
 const printLine = (depth, sign, key, value) => `${tabs(depth)}${sign} ${key}: ${stringify(value, depth + 1)}`;
 
-const stylish = (tree, depth = 1) => {
-  const content = tree.map((child) => {
-    switch (child.state) {
-      case 'deleted':
-        return printLine(depth, '-', child.key, child.value);
-      case 'added':
-        return printLine(depth, '+', child.key, child.value);
-      case 'changed':
-        return `${printLine(depth, '-', child.key, child.valueOfFirstFile)}\n${printLine(depth, '+', child.key, child.valueOfSecondFile)}`;
-      case 'tree':
-        return `${tabs(depth)}  ${child.key}: {\n${stylish(child.tree, depth + 1).join('\n')}\n${tabs(depth)}  }`;
-      case 'unchanged':
-        return printLine(depth, ' ', child.key, child.value);
-      default:
-        throw new Error(`Unknown child state: '${child.state}'!`);
-    }
-  });
-  if (depth === 1) {
-    return `{\n${content.filter((item) => item).join('\n')}\n}`;
-  }
-  return content;
+const stylish = (tree) => {
+  const iter = (node, depth) => {
+    const content = node.map((child) => {
+      switch (child.state) {
+        case 'deleted':
+          return printLine(depth, '-', child.key, child.value);
+        case 'added':
+          return printLine(depth, '+', child.key, child.value);
+        case 'changed':
+          return `${printLine(depth, '-', child.key, child.valueOfFirstFile)}\n${printLine(depth, '+', child.key, child.valueOfSecondFile)}`;
+        case 'tree':
+          return `${tabs(depth)}  ${child.key}: {\n${iter(child.tree, depth + 1)}\n${tabs(depth)}  }`;
+        case 'unchanged':
+          return printLine(depth, ' ', child.key, child.value);
+        default:
+          throw new Error(`Unknown child state: '${child.state}'!`);
+      }
+    });
+    return content.join('\n');
+  };
+  return `{\n${iter(tree, 1)}\n}`;
 };
 
 export default stylish;
