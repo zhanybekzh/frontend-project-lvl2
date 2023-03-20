7f2d987210b51c3dc238ccba4b48d69798752a55
Zhandos <zh.zhanybek@gmail.com>
2023-03-21 00:22:17 +0600

    ошибки линтера

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 import { Command } from 'commander/esm.mjs';
-import genDiff from '../src/lib/index.js';
+import genDiff from '../src/index.js';
 
 const program = new Command();
 
